ba7c0f78e2c9f1c1f43a04ccdb91f24d70e94316
Amit Raj <72662106+kramit624@users.noreply.github.com>
2024-12-27 22:48:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request
 import requests
-import os
-
 
 app = Flask(__name__)
 
@@ -17,7 +15,13 @@ def index():
             repo_status = "Repository not found!"
     return render_template("index.html", repo_status=repo_status)
 
-if _name_ == "_main_":
+if __name__ == "__main__":
+   import os
+
+port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable or default to 5000
+app.run(host="0.0.0.0", port=port)
+ # Add your host and port if necessary, e.g., app.run(host="0.0.0.0", port=5000)
+
     port = 5000  
     if "PORT" in os.environ:
         port = int(os.environ["PORT"])
